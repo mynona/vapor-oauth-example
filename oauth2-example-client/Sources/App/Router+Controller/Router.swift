66f9b76b3b5bb75073122e0ea03e55e7c689353e
mynona <andreas@raumnebenan.de>
2023-12-26 12:16:49 +0100

    Small fixes

--- oauth2-example-client/Sources/App/Router+Controller/Router.swift
+++ oauth2-example-client/Sources/App/Router+Controller/Router.swift
@@ -6,7 +6,7 @@ struct Router: RouteCollection {
       routes.get("client-login", use: Controller().clientLogin)
       routes.get("callback", use: Controller().callback)
       routes.get("introspection", use: Controller().introspection)
-      routes.get("id-token", use: Controller().idToken)
+      routes.get("id-token", use: Controller().idToken) // Doesn't work yet!!!
       routes.get("refresh", use: Controller().refreshToken)
       routes.get("client-logout", use: Controller().clientLogout)
       routes.get(use: Controller().home)
