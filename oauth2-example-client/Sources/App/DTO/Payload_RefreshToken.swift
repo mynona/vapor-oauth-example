eb71d19995f904b293cb985ff52dbcf6bf07d1dc
mynona <andreas@raumnebenan.de>
2024-01-03 16:47:54 +0100

    JWT

--- oauth2-example-client/Sources/App/DTO/Payload_RefreshToken.swift
+++ oauth2-example-client/Sources/App/DTO/Payload_RefreshToken.swift
@@ -2,44 +2,44 @@ import JWTKit
 
 public struct OAuth_RefreshTokenPayload: JWTPayload {
 
-   public var tokenString: String
+   public var jti: String
    public var clientID: String
    public var userID: String?
    public var scopes: [String]?
-   public var expiration: Date
+   public var exp: Date
    public var issuer: String
    public var issuedAt: Date
 
    enum CodingKeys: String, CodingKey {
-      case tokenString = "jti" // unique token id
+      case jti = "jti" // unique token id
       case clientID = "aud" // audience
       case userID = "sub" // subject
-      case scopes = "scopes"
-      case expiration = "exp" // expiration
+      case scopes = "_scopes"
+      case exp = "exp" // expiration
       case issuer = "iss" // issuer
       case issuedAt = "iat" // issuing date
    }
 
    init(
-      tokenString: String,
+      jti: String,
       clientID: String,
       userID: String? = nil,
       scopes: [String]? = nil,
-      expiration: Date,
+      exp: Date,
       issuer: String,
       issuedAt: Date
    ) {
-      self.tokenString = tokenString
+      self.jti = jti
       self.clientID = clientID
       self.userID = userID
       self.scopes = scopes
-      self.expiration = expiration
+      self.exp = exp
       self.issuer = issuer
       self.issuedAt = issuedAt
    }
 
    public func verify(using signer: JWTSigner) throws {
-       try expiration.verifyNotExpired()
+       try exp.verifyNotExpired()
    }
 
 }
